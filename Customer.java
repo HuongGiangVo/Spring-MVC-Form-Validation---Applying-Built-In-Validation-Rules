2dc3c1ac841993b865ddba62dbd8dabe8aa11c1e
Huong Giang Vo <51000008+HuongGiangVo@users.noreply.github.com>
2020-11-10 09:39:22 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,6 +1,9 @@
 package springdemo;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -9,6 +12,14 @@ public class Customer {
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
+	
+	@Max(value=10, message="must be less than or equal to 10")
+	@Min(value=0, message="must be greater than or equal to zero")
+	@NotNull(message="is required")
+	private Integer freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")//char a to z or A-Z digit 0 to 9, 5:  only 5 chars/digits
+	private String postalCode;
 	
 	public String getFirstName() {
 		return firstName;
@@ -21,6 +32,18 @@ public class Customer {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 	
 
